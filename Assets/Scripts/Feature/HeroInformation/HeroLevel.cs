04af334baec8d52a36a738ee2a13691849bf66a3
Duy Nguyen <86402701+DuyAlann@users.noreply.github.com>
2023-09-11 01:20:04 +0700

    feat: hero table property

--- Assets/Scripts/Feature/HeroInformation/HeroLevel.cs
+++ Assets/Scripts/Feature/HeroInformation/HeroLevel.cs
@@ -8,12 +8,21 @@ namespace Feature.HeroInformation
     public class HeroLevel: MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI levelText;
+        [SerializeField] private TextMeshProUGUI hpText;
+        [SerializeField] private TextMeshProUGUI atkText;
+        [SerializeField] private TextMeshProUGUI defText;
+        [SerializeField] private TextMeshProUGUI rangeText;
+        
         [SerializeField] private Image experienceBarImage;
 
         private void Awake()
         {
-            SetExperienceBarSize(.5f);
+            SetExperienceBarSize(.9f);
             SetLevelNumber(7);
+            SetHealthPointNumber(1000);
+            SetAttackNumber(200);
+            SetDefenseNumber(80);
+            SetRangeNumber(2.32);
         }
 
         private void SetExperienceBarSize(float experienceNormalized)
@@ -23,7 +32,27 @@ namespace Feature.HeroInformation
 
         private void SetLevelNumber(int levelNumber)
         {
-            levelText.text = "Level: " + (levelNumber + 1);
+            levelText.text = (levelNumber).ToString();
+        }
+        
+        private void SetHealthPointNumber(int hpNumber)
+        {
+            hpText.text = (hpNumber).ToString();
+        }
+        
+        private void SetAttackNumber(int atkNumber)
+        {
+            atkText.text = (atkNumber).ToString();
+        }
+        
+        private void SetDefenseNumber(int defNumber)
+        {
+            defText.text = (defNumber).ToString();
+        }
+        
+        private void SetRangeNumber(double rangeNumber)
+        {
+            rangeText.text = (rangeNumber).ToString("F2");
         }
     }
 }
