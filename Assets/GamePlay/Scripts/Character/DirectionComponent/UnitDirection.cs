27e988b2d1cec921bdda491624b5fcc1cf1916f9
HuyHoangPham <phamhuyhoang3004@gmail.com>
2023-11-04 16:33:30 +0700

    fix: fix direction by scale not by flip.x

--- Assets/GamePlay/Scripts/Character/DirectionComponent/UnitDirection.cs
+++ Assets/GamePlay/Scripts/Character/DirectionComponent/UnitDirection.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 
 public class UnitDirection : UnitBaseComponent
 {
-    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Transform _transform;
     private Vector3 _prePos;
     private Vector3 _curPos;
 
     private bool _isFocusTarget;
     private UnitBase _target;
+    private float _localScaleX;
     
     #region Core
+    private void Start() => _localScaleX = _transform.localScale.x;
     private void OnEnable()
     {
         _unitBaseParent.OnTargetChanging += OnTargetChanging;
@@ -28,13 +30,14 @@ public class UnitDirection : UnitBaseComponent
         _curPos = this.gameObject.transform.position;
         if (_target)
         {
-            isTurnOnFlipX = !CheckLeftToRightDirection(_curPos,_target.transform.position);
+            isTurnOnFlipX = CheckLeftToRightDirection(_curPos,_target.transform.position);
         }
         else
         {
-            isTurnOnFlipX = !CheckLeftToRightDirection(_prePos,_curPos);
+            isTurnOnFlipX = CheckLeftToRightDirection(_prePos,_curPos);
         }
-        _spriteRenderer.flipX = isTurnOnFlipX;
+        var localScale = _transform.localScale;
+        _transform.localScale = isTurnOnFlipX ? new Vector3(_localScaleX, localScale.y, localScale.z) : new Vector3(-_localScaleX, localScale.y, localScale.z);
         _prePos = _curPos;  
     }
     private void OnTargetChanging(UnitBase target) => _target = target;
