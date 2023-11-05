f1d51d7f08178c5bfc37d62760db1fee979aea73
HuyHoangPham <phamhuyhoang3004@gmail.com>
2023-11-05 14:51:34 +0700

    feat: add stage inventory config

--- Assets/GamePlay/Scripts/Inventory/InGameInventoryDataAsset.cs
+++ Assets/GamePlay/Scripts/Inventory/InGameInventoryDataAsset.cs
@@ -7,13 +7,25 @@ public class InGameInventoryDataAsset : ScriptableObject
     [SerializeField] private int _life;
 
     public int GetCurrencyValue() => _currency;
-    public int GetLifeValue() => _currency;
+    public int GetLifeValue() => _life;
     
     public void TryChangeCurrency(int value)
     {
         if (_currency > value)
         {
             _currency += value;
+        }
+    }
+    public void TryChangeLife(int value)
+    {
+        if (_life > value)
+        {
+            _life += value;
+        }
+        if (_life <= 0)
+        {
+            //todo
+            //End game condition
         }
     }
 }
--- Assets/GamePlay/Scripts/Stage/StageDataAsset.cs
+++ Assets/GamePlay/Scripts/Stage/StageDataAsset.cs
@@ -14,16 +14,16 @@ public struct StageConfig
     public TowerKitSetConfig TowerKitSetConfig;
     public RouteSetConfig RouteSetConfig;
     public StageSpawningConfig StageSpawningConfig;
+    public InGameInventoryDataAsset InGameInventoryDataAsset;
 }
 
 [CreateAssetMenu(fileName = "StageDataAsset", menuName = "ScriptableObject/Database/Stage/StageDataAsset")]
 public class StageDataAsset : ScriptableObject
 {
     [SerializeField] private StageId _currentStageId;
-    public StageId CurrentStageId() => _currentStageId;
     [SerializeField] [SerializedDictionary("StageId","StageConfig")]
     private SerializedDictionary<StageId,StageConfig> _stageDict;
-    
+    public StageId CurrentStageId() => _currentStageId;
     
     public StageConfig GetStageConfig(StageId stageId = StageId.CHAP_1_STAGE_1)
     {
