2b36d21852a0880bd79ac86dd6b1665b32f67eb9
Duy Nguyen <86402701+DuyAlann@users.noreply.github.com>
2023-11-06 01:47:20 +0700

    feat: Hero mode view and view model

---
+++ Assets/Scripts/Feature/HeroInformation/HeroModeView.cs
@@ -0,0 +1,44 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Feature.HeroInformation
+{
+    public class HeroModeView : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private MenuCharacterModes _menuType;
+
+        private TextMeshProUGUI _text;
+        private Action<MenuCharacterModes> _onButtonSelected;
+
+        public MenuCharacterModes MenuType { get; private set; }
+        public void Initialize(Action<MenuCharacterModes> onButtonSelected)
+        {
+            _onButtonSelected = onButtonSelected;
+            _text = _button.GetComponentInChildren<TextMeshProUGUI>();
+            _button.onClick.AddListener(OnButtonClick);
+        }
+        
+        public void SetMenuType(MenuCharacterModes menuType)
+        {
+            MenuType = menuType;
+        }
+
+        private void OnButtonClick()
+        {
+            _onButtonSelected?.Invoke(_menuType);
+        }
+
+        // Call this method to update the button's visual state
+        public void UpdateVisualState(Sprite positiveImage, Color positiveColor)
+        {
+            _button.image.sprite = positiveImage;
+            if (_text != null)
+            {
+                _text.color = positiveColor;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Feature/HeroInformation/HeroModeViewModel.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Feature.HeroInformation
+{
+    // Enum for the different character menu modes
+    public enum MenuCharacterModes
+    {
+        Skill,
+        Skin,
+        History
+    }
+
+    public class HeroModeViewModel : MonoBehaviour
+    {
+        [SerializeField] private List<HeroModeView> _buttonViewModels;
+        [SerializeField] private List<GameObject> _objContentList;
+
+        [SerializeField] private Sprite _positiveImage;
+        [SerializeField] private Sprite _absoluteImage;
+
+        private Color _positiveColor;
+        private Color _absoluteColor;
+
+        private readonly string _hexPositiveColor = "#F3EF94";
+        private readonly string _hexAbsoluteColor = "#323232";
+
+        // Mapping of menu type to button view model and content object
+        private Dictionary<MenuCharacterModes, (HeroModeView viewModel, GameObject contentObject)> _menuMapping;
+
+        private void Start()
+        {
+            if (ColorUtility.TryParseHtmlString(_hexPositiveColor, out _positiveColor) &&
+                ColorUtility.TryParseHtmlString(_hexAbsoluteColor, out _absoluteColor))
+            {
+                InitializeButtonViewModels();
+                CreateMenuMapping();
+                SetInitialContentState();
+            }
+            else
+            {
+                Debug.LogError("Invalid color strings.");
+            }
+        }
+
+        private void InitializeButtonViewModels()
+        {
+            foreach (var viewModel in _buttonViewModels)
+            {
+                viewModel.Initialize(HandleButtonSelected);
+            }
+        }
+
+        private void CreateMenuMapping()
+        {
+            _menuMapping = new Dictionary<MenuCharacterModes, (HeroModeView, GameObject)>();
+
+            for (int i = 0; i < _objContentList.Count; i++)
+            {
+                var menuType = _buttonViewModels[i].MenuType; // Assuming ButtonModeViewModel has a MenuType property of type MenuCharacterModes
+                _menuMapping[menuType] = (_buttonViewModels[i], _objContentList[i]);
+            }
+        }
+
+        private void SetInitialContentState()
+        {
+            foreach (var content in _objContentList)
+            {
+                content.SetActive(false);
+            }
+            if (_objContentList.Count > 0)
+            {
+                _objContentList[0].SetActive(true);
+            }
+        }
+
+        private void HandleButtonSelected(MenuCharacterModes menuType)
+        {
+            UpdateButtonVisualStates(menuType);
+            SwapContent(menuType);
+        }
+
+        private void UpdateButtonVisualStates(MenuCharacterModes activeMenuType)
+        {
+            foreach (var pair in _menuMapping)
+            {
+                var viewModel = pair.Value.viewModel;
+                var targetSprite = pair.Key == activeMenuType ? _positiveImage : _absoluteImage;
+                var targetColor = pair.Key == activeMenuType ? _positiveColor : _absoluteColor;
+                viewModel.UpdateVisualState(targetSprite, targetColor);
+            }
+        }
+
+        private void SwapContent(MenuCharacterModes menuType)
+        {
+            foreach (var pair in _menuMapping)
+            {
+                pair.Value.contentObject.SetActive(pair.Key == menuType);
+            }
+        }
+    }
+}
