50ea6fc80569fb967bc6fa0b5055f133ff791c18
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-02-15 18:49:41 +0700

    feat: checking skill finished

--- Assets/GamePlay/Scripts/Character/StateMachine/HeroStateMachine/HeroExecuteActiveSkillState.cs
+++ Assets/GamePlay/Scripts/Character/StateMachine/HeroStateMachine/HeroExecuteActiveSkillState.cs
@@ -4,6 +4,8 @@ public class HeroExecuteActiveSkillState : CharacterBaseState
 {
     private readonly BaseHeroStateMachine _context;
     private static readonly int IsPlayActiveSkill = Animator.StringToHash("IsPlayActiveSkill");
+    private float _skillDuringTime;
+    private bool _isFinished;
     public HeroExecuteActiveSkillState(BaseHeroStateMachine currentContext) : base(currentContext)
     {
         IsRootState = true;
@@ -11,11 +13,14 @@ public class HeroExecuteActiveSkillState : CharacterBaseState
     }
     public override void EnterState()
     {
+        _isFinished = false;
+        _skillDuringTime = _context.CharacterAnimator.GetCurrentAnimatorClipInfo(0).Length;
         _context.CharacterAnimator.SetBool(IsPlayActiveSkill, true);
     }
     public override void UpdateState()
     {
         CheckSwitchState();
+        CheckEndedSkill();
     }
     public override void ExitState()
     {
@@ -23,9 +28,26 @@ public class HeroExecuteActiveSkillState : CharacterBaseState
     }
     public override void CheckSwitchState()
     {
-        if (!_context.UserActionController.IsInAction())
+        if (_context.IsDie)
+        {
+            _context.CurrentState.SwitchState(_context.StateFactory.GetState(CharacterState.Die));
+        }
+        if (_isFinished)
         {
             _context.CurrentState.SwitchState(_context.StateFactory.GetState(CharacterState.Idle));
+        }
+        // if (!_context.UserActionController.IsInAction())
+        // {
+        //     _context.CurrentState.SwitchState(_context.StateFactory.GetState(CharacterState.Idle));
+        // }
+    }
+    private void CheckEndedSkill()
+    {
+        _skillDuringTime -= Time.deltaTime;
+        if (_skillDuringTime <= 0)
+        {
+            _context.UserActionController.SetFinishedUserAction();
+            _isFinished = true;
         }
     }
     public override void InitializeSubState()
