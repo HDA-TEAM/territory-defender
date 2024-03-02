dbc53f8018b4c90125ac5357448b8aa74021ec98
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-03-02 16:46:58 +0700

    feat: remove old resource (cont)

--- Assets/Features/Common/Scripts/ResourceKey.cs
+++ Assets/Features/Common/Scripts/ResourceKey.cs
@@ -2,14 +2,14 @@ public static class ResourceKey
     {
         public static class Prefabs
         {
-            public const string HomeScreen = "TD/Home/HomeScreen";
-            public const string HistoryPu = "TD/Home/HistoryPu";
-            public const string DictionaryPu = "TD/Home/DictionaryPu";
-            public const string MasteryPu = "TD/Home/MasteryPu";
-            public const string StageInformationPu = "TD/Home/StageInformationPu";
-            public const string HeroInformationPu = "TD/Home/HeroInformationPu";
-            public const string QuestPu = "TD/Home/QuestPu";
-            public const string SettingPu = "TD/Home/SettingPu";
+            public const string HomeScreen = "Home/HomeScreen";
+            public const string HistoryPu = "Home/HistoryPu";
+            public const string DictionaryPu = "Home/DictionaryPu";
+            public const string MasteryPu = "Home/MasteryPu";
+            public const string StageInformationPu = "Home/StageInformationPu";
+            public const string HeroInformationPu = "Home/HeroInformationPu";
+            public const string QuestPu = "Home/QuestPu";
+            public const string SettingPu = "Home/SettingPu";
         }
 
         public static class Textures
