e0ad4fdbd5782760bca7e8f1eca15b687920da88
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-03-16 21:43:08 +0700

    feat: update arrow route line

--- Assets/GamePlay/Scripts/Character/ProjectileDataAsset.cs
+++ Assets/GamePlay/Scripts/Character/ProjectileDataAsset.cs
@@ -5,8 +5,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public enum EProjectileType{
-    Arrow = 1, 
+public enum EProjectileType
+{
+    Arrow = 1,
     WaterBomb = 2,
 }
 
@@ -27,7 +28,7 @@ public class ProjectileDataAsset : ScriptableObject
         var bullet = PoolingController.Instance.SpawnObject(unitId);
         return bullet.GetComponent<ProjectileBase>();
     }
-    
+
     // [SerializeField] private AnimationCurve bowAttackCurve;
     // public void GetLineRoute(Vector2 posSpawn, BulletType bulletType,UnitBase target)
     // {
@@ -51,33 +52,33 @@ public interface IProjectileLineRoute
 // {
 //     public void ApplyLineRoute(GameObject curWeapon, UnitBase target, AnimationCurve customCurve)
 //     {
-        // curWeapon.transform.DOMove(target.transform.position, 0.25f).SetEase(Ease.Linear)
-        //     .OnComplete(() =>
-        //     {
-        //         target.GetComponent<HealthComp>().PlayHurting(10);
-        //         curWeapon.SetActive(false);
-        //     });
-        // transform.DOPath(new Vector3[] { pathPoints[0].position, pathPoints[1].position, pathPoints[2].position, pathPoints[3].position },
-        //         duration, PathType.CubicBezier)
-        //     .SetOptions(false)
-        //     .SetEase(Ease.Linear)
-        //     .OnComplete(OnMovementComplete);
-        // return curWeapon.transform.DOPath(
-        //         new Vector3[]
-        //         {
-        //             target.transform.position,
-        //             curWeapon.transform.position + Vector3.up * 10,
-        //             (curWeapon.transform.position + target.transform.position) / 2 + Vector3.up * 10,
-        //         },
-        //         0.1f, PathType.CubicBezier)
-        //     .SetOptions(false)
-        //     .SetEase(Ease.Linear)
-        //     .OnComplete(
-        //         () =>
-        //         {
-        //             target.GetComponent<HealthComp>().PlayHurting(10);
-        //             curWeapon.SetActive(false);
-        //         });
+// curWeapon.transform.DOMove(target.transform.position, 0.25f).SetEase(Ease.Linear)
+//     .OnComplete(() =>
+//     {
+//         target.GetComponent<HealthComp>().PlayHurting(10);
+//         curWeapon.SetActive(false);
+//     });
+// transform.DOPath(new Vector3[] { pathPoints[0].position, pathPoints[1].position, pathPoints[2].position, pathPoints[3].position },
+//         duration, PathType.CubicBezier)
+//     .SetOptions(false)
+//     .SetEase(Ease.Linear)
+//     .OnComplete(OnMovementComplete);
+// return curWeapon.transform.DOPath(
+//         new Vector3[]
+//         {
+//             target.transform.position,
+//             curWeapon.transform.position + Vector3.up * 10,
+//             (curWeapon.transform.position + target.transform.position) / 2 + Vector3.up * 10,
+//         },
+//         0.1f, PathType.CubicBezier)
+//     .SetOptions(false)
+//     .SetEase(Ease.Linear)
+//     .OnComplete(
+//         () =>
+//         {
+//             target.GetComponent<HealthComp>().PlayHurting(10);
+//             curWeapon.SetActive(false);
+//         });
 //     }
 // }
 
@@ -87,32 +88,34 @@ public class ProjectileTrajectoryRouteLine : IProjectileLineRoute
     {
         float t = 0f;
         Vector3 prevBulletPos = curWeapon.transform.position;
+        Vector3 midPoint = (curWeapon.transform.position + target.transform.position) / 2f;
+        midPoint += Vector3.up / 3;
         DOTween.To(() => t, x => t = x, 1f, duration)
-            .SetEase(customCurve)
+            // .SetEase(customCurve)
             .OnUpdate(() =>
             {
+
                 // Calculate the position of the arrow based on the Bezier curve equation.
                 Vector3 newPosition = CalculateBezierPoint(
                     t,
                     curWeapon.transform.position,
-                    curWeapon.transform.position + Vector3.up / 3f ,
+                    midPoint,
                     target.transform.position
-                    
                 );
-                
                 // Update the arrow's position.
                 curWeapon.transform.position = newPosition;
-                
+
                 // Update the arrow's angle
                 float zAngle = VectorUtility.GetZAngleOfTwoPoint(prevBulletPos, newPosition);
-                curWeapon.transform.rotation = Quaternion.Euler(0f,0f, zAngle);
-                    
-                 //Update previous pos of bullet
+                curWeapon.transform.rotation = Quaternion.Euler(0f, 0f, zAngle);
+                Debug.Log(newPosition + " " + curWeapon.transform.rotation.z);
+                //Update previous pos of bullet
                 prevBulletPos = newPosition;
-                
+
             })
             .OnComplete(callback);
     }
+
     private Vector3 CalculateBezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2)
     {
         // Bezier curve equation: B(t) = (1-t)^2 * P0 + 2 * (1-t) * t * P1 + t^2 * P2
