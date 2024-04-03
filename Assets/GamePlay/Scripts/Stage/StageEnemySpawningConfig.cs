12bacb1e7717d11bd1b7569cabd0d8f717f6c85d
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-04-03 23:26:36 +0700

    feat: parse StageEnemySpawningConfig to json

--- Assets/GamePlay/Scripts/Stage/StageEnemySpawningConfig.cs
+++ Assets/GamePlay/Scripts/Stage/StageEnemySpawningConfig.cs
@@ -1,25 +1,42 @@
+using CustomInspector;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 [CreateAssetMenu(fileName = "StageEnemySpawningConfig", menuName = "ScriptableObject/Database/Stage/SpawningConfigData")]
-[Serializable]
+[Serializable,Preserve]
 public class StageEnemySpawningConfig : ScriptableObject
 {
+    [Button("ParseToJson")]
+    [Button("ReadData")]
     public StageId StageId;
     public List<WaveSpawning> WavesSpawning;
-    [Serializable]
+    public List<WaveSpawning> TestWavesSpawning;
+    [Serializable,Preserve]
     public struct WaveSpawning
     {
         public List<GroupSpawning> GroupsSpawning;
     }
 
-    [Serializable]
+    [Serializable,Preserve]
     public struct GroupSpawning
     {
         public float StartSpawning;
         public UnitId.Enemy ObjectSpawn;
         public int RouteId;
         public int NumberSpawning;
+    }
+
+    
+    [SerializeField] private string _data;
+    public void ParseToJson()
+    {
+        _data = JsonConvert.SerializeObject(WavesSpawning);
+    }
+    public void ReadData()
+    {
+        TestWavesSpawning = JsonConvert.DeserializeObject<List<WaveSpawning>>(_data);
     }
 }
