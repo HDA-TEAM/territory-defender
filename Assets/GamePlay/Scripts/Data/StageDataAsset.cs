d2e359b6f6e94dee1e98743c74d293298f26fb08
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-05-08 13:39:03 +0700

    faet: init StageDataAsset

--- Assets/GamePlay/Scripts/Data/StageDataAsset.cs
+++ Assets/GamePlay/Scripts/Data/StageDataAsset.cs
@@ -1,21 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace GamePlay.Scripts.Data
 {
-
+    public struct StagePassed
+    {
+        public int TotalStar;
+        public StageId StageId;
+    }
     public struct StageDataModel : IDefaultDataModel
     {
+        public List<StagePassed> ListStagePassed;
         public bool IsEmpty()
         {
             return false;
         }
         public void SetDefault()
         {
+            ListStagePassed = new List<StagePassed>();
         }
     }
     
     [CreateAssetMenu(fileName = "StageDataAsset", menuName = "ScriptableObject/Database/Stage/StageDataAsset")]
-    public class StageDataAsset : ScriptableObject
+    public class StageDataAsset : BaseDataAsset<StageDataModel>
     {
+        public List<StagePassed> ListStagePassed
+        {
+            get
+            {
+                return _model.ListStagePassed ?? (_model.ListStagePassed = new List<StagePassed>());
+            }
+        }
+        public void AddStagePassed(StagePassed stagePassed)
+        {
+            _model.ListStagePassed.Add(stagePassed);
+            SaveData();
+        }
     }
 }
