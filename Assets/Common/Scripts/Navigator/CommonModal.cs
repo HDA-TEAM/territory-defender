37a81e7a30f175dcd3036667ee95e85cee19eb7b
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-05-08 16:03:14 +0700

    Update CommonModal.cs

--- Assets/Common/Scripts/Navigator/CommonModal.cs
+++ Assets/Common/Scripts/Navigator/CommonModal.cs
@@ -7,13 +7,13 @@ namespace Common.Scripts.Navigator
     public class CommonModal : Modal
     {
         [SerializeField] private Button[] _btnCloses;
-        private void Awake()
+        protected virtual void Awake()
         {
             foreach (var btn in _btnCloses)
             {
                 btn.onClick.AddListener(PopModal);
             }
         }
-        private void PopModal() => NavigatorController.PopModal();
+        protected virtual void PopModal() => NavigatorController.PopModal();
     }
 }
