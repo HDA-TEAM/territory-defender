8054c8d0d606d8521ba05a2b59c98030848b77fa
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-05-11 15:26:47 +0700

    faet: update show unit avatar in UnitInformationPanel

--- Assets/GamePlay/Scripts/Menu/InGameStageScreen/UnitInformationPanel/UnitAvatarView.cs
+++ Assets/GamePlay/Scripts/Menu/InGameStageScreen/UnitInformationPanel/UnitAvatarView.cs
@@ -14,7 +14,18 @@ namespace GamePlay.Scripts.Menu.UnitInformationPanel
         public void Setup(ShowStatsInformationComposite statsInformationComposite)
         {
             _txtName.text = statsInformationComposite.Name;
-            Debug.Log(_totalUnitDataConfig.GetSingleUnitDataConfig(UnitId.Hero.TrungTrac.ToString()).UnitBase.name);
+            UnitDataComposite unitDataComposite = _totalUnitDataConfig.GetSingleUnitDataConfig(UnitId.Hero.TrungTrac.ToString());
+            if (unitDataComposite.UnitSprites.AvatarIcon)
+            {
+                _avatar.sprite = unitDataComposite.UnitSprites.AvatarIcon;
+                _avatar.SetNativeSize();
+                RectTransform avatarRect = _avatar.GetComponent<RectTransform>();
+                avatarRect.anchorMax = new Vector2(0.5f, 0.5f);
+                avatarRect.anchorMin = new Vector2(0.5f, 0.5f);
+                avatarRect.anchoredPosition = Vector2.zero;
+            }
+            
+            Debug.Log(unitDataComposite.UnitBase.name);
         }
     }
 }
