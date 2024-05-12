efa4842b3b41d4d4db284809c7be2848f0c94be9
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-05-12 14:59:36 +0700

    fix: resolve namespace bug

--- Assets/Features/StageInfo/Scripts/StageInfoView/StageInfoViewModel.cs
+++ Assets/Features/StageInfo/Scripts/StageInfoView/StageInfoViewModel.cs
@@ -1,76 +1,78 @@
-using System.Collections.Generic;
 using Common.Loading.Scripts;
-using Common.Scripts;
 using GamePlay.Scripts.Data;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class StageInfoViewModel : MonoBehaviour
+namespace Features.StageInfo.Scripts.StageInfoView
 {
-    [Header("UI")] [SerializeField] private ItemPlayView _itemPlayView;
-    [SerializeField] private ItemMasteryView _itemMasteryView;
-    [SerializeField] private List<ItemStageStarView> _itemStageStarViews;
-    [SerializeField] private StageInfoDetailView _stageInfoDetailView;
-
-    [Header("Data")] [SerializeField] private GameModeViewModel _gameModeViewModel;
-
-    [SerializeField] private ListHeroChooseViewModel _heroChooseView;
-
-    // Internal
-    private static StageComposite _currentStage;
-
-    private void Awake()
+    public class StageInfoViewModel : MonoBehaviour
     {
-        _itemPlayView.Setup(OnSelectedItemPlay);
-        _itemMasteryView.Setup(OnSelectedItemMastery);
+        [Header("UI")] [SerializeField] private ItemPlayView _itemPlayView;
+        [SerializeField] private ItemMasteryView _itemMasteryView;
+        [SerializeField] private List<ItemStageStarView> _itemStageStarViews;
+        [SerializeField] private StageInfoDetailView _stageInfoDetailView;
 
-        UpdateData();
-    }
+        [Header("Data")] [SerializeField] private GameModeViewModel _gameModeViewModel;
 
-    private void UpdateData()
-    {
-        _currentStage = StageDataManager.Instance.CurrentStage;
-        UpdateView();
-    }
+        [SerializeField] private ListHeroChooseViewModel _heroChooseView;
 
-    private void UpdateView()
-    {
-        // Fill the star result for that stage
-        for (int i = 0; i < _itemStageStarViews.Count; i++)
+        // Internal
+        private static StageComposite _currentStage;
+
+        private void Awake()
         {
-            if (i < _currentStage.StageStar)
-                _itemStageStarViews[i].SetupYellowStar();
+            _itemPlayView.Setup(OnSelectedItemPlay);
+            _itemMasteryView.Setup(OnSelectedItemMastery);
 
-            else
-                _itemStageStarViews[i].SetupGrownStar();
+            UpdateData();
         }
 
-        _stageInfoDetailView.Setup(_currentStage);
-    }
-
-    private void OnSelectedItemMastery(ItemMasteryView itemMasteryView)
-    {
-        var stateMachine = UIManagerStateMachine.Instance;
-        stateMachine.ChangeModalState<MasteryPagePuState>();
-    }
-
-    private void OnSelectedItemPlay(ItemPlayView itemPlayView)
-    {
-        GameMode currentGameMode = _gameModeViewModel.GetMode();
-        HeroComposite heroBeChosen = _heroChooseView.GetHeroChoose();
-
-        //TODO: load the map is suitable with the (Stage, Mode, Hero Chosen)
-        LoadSceneBasedOnStageAndMode(_currentStage, currentGameMode, heroBeChosen);
-    }
-
-    private void LoadSceneBasedOnStageAndMode(StageComposite stage, GameMode gameMode, HeroComposite hero)
-    {
-        Debug.Log($"StageId: {stage.StageId}, GameMode: {gameMode}, Hero : {hero.Name}");
-        LoadingSceneController.Instance.LoadingHomeToGame(new StartStageComposite
+        private void UpdateData()
         {
-            // Demo
-            StageId = StageId.Chap1Stage0,
-            StageDiff = StageDiff.Normal,
-            HeroId = hero.HeroId,
-        });
+            _currentStage = StageDataManager.Instance.CurrentStage;
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            // Fill the star result for that stage
+            for (int i = 0; i < _itemStageStarViews.Count; i++)
+            {
+                if (i < _currentStage.StageStar)
+                    _itemStageStarViews[i].SetupYellowStar();
+
+                else
+                    _itemStageStarViews[i].SetupGrownStar();
+            }
+
+            _stageInfoDetailView.Setup(_currentStage);
+        }
+
+        private void OnSelectedItemMastery(ItemMasteryView itemMasteryView)
+        {
+            var stateMachine = UIManagerStateMachine.Instance;
+            stateMachine.ChangeModalState<MasteryPagePuState>();
+        }
+
+        private void OnSelectedItemPlay(ItemPlayView itemPlayView)
+        {
+            GameMode currentGameMode = _gameModeViewModel.GetMode();
+            HeroComposite heroBeChosen = _heroChooseView.GetHeroChoose();
+
+            //TODO: load the map is suitable with the (Stage, Mode, Hero Chosen)
+            LoadSceneBasedOnStageAndMode(_currentStage, currentGameMode, heroBeChosen);
+        }
+
+        private void LoadSceneBasedOnStageAndMode(StageComposite stage, GameMode gameMode, HeroComposite hero)
+        {
+            Debug.Log($"StageId: {stage.StageId}, GameMode: {gameMode}, Hero : {hero.Name}");
+            LoadingSceneController.Instance.LoadingHomeToGame(new StartStageComposite
+            {
+                // Demo
+                StageId = StageId.Chap1Stage0,
+                StageDiff = StageDiff.Normal,
+                HeroId = hero.HeroId,
+            });
+        }
     }
 }
