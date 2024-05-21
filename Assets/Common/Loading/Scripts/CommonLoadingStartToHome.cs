80f3648d9f3920bd44f09160e1bc27fe53b10458
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-05-21 16:47:43 +0700

    feat: loading all dataasset at loading start to home

--- Assets/Common/Loading/Scripts/CommonLoadingStartToHome.cs
+++ Assets/Common/Loading/Scripts/CommonLoadingStartToHome.cs
@@ -1,5 +1,6 @@
 using Cysharp.Threading.Tasks;
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,20 +9,51 @@ namespace Common.Loading.Scripts
     [CreateAssetMenu(fileName = "CommonLoadingStartToHome", menuName = "ScriptableObject/LoadingScene/CommonLoadingStartToHome")]
     public class CommonLoadingStartToHome : CommonLoadingScene
     {
-        public override async void StartLoading(Action onCompleted, IProgress<float> progress)
+        private async UniTask LoadingAllLocalData()
         {
-            
+            foreach (var localData in _localDataList)
+            {
+                localData.LoadData();
+                Debug.Log("Load " + localData.name);
+                await UniTask.WaitUntil(localData.IsDoneLoadData);
+                Debug.Log("Done Load " + localData.name);
+            }
+        }
+        private async UniTask LoadingScene()
+        {
+            Debug.Log("LoadingScene ");
             string sceneLoadingName = SceneIdentified.GetSceneName(ESceneIdentified.Home);
             await SceneManager.LoadSceneAsync(sceneLoadingName, LoadSceneMode.Single);
             SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneLoadingName));
-            // todo Wtf in here : why can't unload LoadingScene
-            // SceneManager.UnloadSceneAsync(SceneIdentified.GetSceneName(ESceneIdentified.LoadingScene));
-
-            for (int i = 0; i <= 10; i++)
+            await UniTask.WaitUntil(() => SceneManager.GetActiveScene().name == sceneLoadingName);
+            Debug.Log("Done LoadingScene ");
+        }
+        public override async void StartLoading(Action onCompleted, IProgress<float> progress)
+        {
+            _progress = progress;
+            _loadingSteps = new List<LoadingStep>
             {
-                progress.Report(i * 0.1f);
-                await UniTask.Delay(TimeSpan.FromSeconds(0.2f));
-            }
+                new LoadingStep
+                {
+                    Percent = 0.5f,
+                    OnAction = LoadingAllLocalData,
+                    MinDelayNextStepDuration = 0.5f,
+                },
+                new LoadingStep
+                {
+                    Percent = 0.4f,
+                    OnAction = LoadingScene,
+                    MinDelayNextStepDuration = 0.4f,
+                },
+                new LoadingStep
+                {
+                    Percent = 0.1f,
+                    OnAction = () => new UniTask(),
+                    MinDelayNextStepDuration = 0.1f,
+                }
+            };
+
+            await ExecuteLoadingStep();
             onCompleted?.Invoke();
         }
     }
