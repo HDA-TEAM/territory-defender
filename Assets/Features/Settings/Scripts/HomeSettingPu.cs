a40a20601caf70a8e99dbb47f8e95ece9a0e7a4c
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-05-26 11:18:24 +0700

    feat: add home menu setting pu

---
+++ Assets/Features/Settings/Scripts/HomeSettingPu.cs
@@ -0,0 +1,11 @@
+using Common.Scripts;
+
+namespace Features.Settings.Scripts
+{
+    public class HomeSettingPu : CommonSettingPu
+    {
+        protected override void PopModal()
+        {
+        }
+    }
+}
