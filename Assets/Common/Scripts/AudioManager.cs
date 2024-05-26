2a1459c4ad18b4f12dd404db3269b1c3d85bc901
HuyHoangPham <phamhuyhoang3004@gmail.com>
2024-05-26 16:24:49 +0700

    feat: setup sound and music

--- Assets/Common/Scripts/AudioManager.cs
+++ Assets/Common/Scripts/AudioManager.cs
@@ -1,3 +1,5 @@
+using Cysharp.Threading.Tasks;
+using GamePlay.Scripts.Data;
 using SuperMaxim.Messaging;
 using UnityEngine;
 
@@ -15,16 +17,32 @@ namespace Common.Scripts
     {
         [SerializeField] private AudioSource _audioSourceLoop;
         [SerializeField] private AudioSource _audioSourceOneShot;
-
-        private void Awake()
+        [SerializeField] private SettingDataAsset _settingDataAsset;
+        private async void Awake()
         {
             Messenger.Default.Subscribe<AudioPlayOneShotPayload>(PlayOneShot);
             Messenger.Default.Subscribe<AudioPlayLoopPayload>(PlayLoop);
+            _settingDataAsset.OnChangeSound += OnChangeSound;
+            _settingDataAsset.OnChangeMusic += OnChangeMusic;
+
+            await UniTask.WaitUntil(() => _settingDataAsset.IsDoneLoadData());
+            OnChangeSound(_settingDataAsset.IsSoundOn);
+            OnChangeMusic(_settingDataAsset.IsMusicOn);
         }
         private void OnDestroy()
         {
             Messenger.Default.Unsubscribe<AudioPlayOneShotPayload>(PlayOneShot);
             Messenger.Default.Unsubscribe<AudioPlayLoopPayload>(PlayLoop);
+            _settingDataAsset.OnChangeSound -= OnChangeSound;
+            _settingDataAsset.OnChangeMusic -= OnChangeMusic;
+        }
+        private void OnChangeSound(bool isTurnOn)
+        {
+            _audioSourceOneShot.mute = !isTurnOn;
+        }
+        private void OnChangeMusic(bool isTurnOn)
+        {
+            _audioSourceLoop.mute = !isTurnOn;
         }
         private void PlayOneShot(AudioPlayOneShotPayload payload)
         {
